00727049518d1b4a1f3ebbdb2973514a6e9b0a4b
Usama Alian <engalian@gmail.com>
2023-12-29 07:09:00 -0600

    Add files via upload

---
+++ master-reading-03-remove-white-space.py
@@ -0,0 +1,7 @@
+
+with open('bkp-first-names.txt','r') as file:
+    for sater in file:
+        print(sater.strip()," ",end='')  # read line by line
+        #print(len(sater))  # read line by line
+        
+
